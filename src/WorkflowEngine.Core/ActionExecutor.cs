f888bf351e81e1b07cd11df6a5a268ff89270054
Poul Kjeldager Sørensen <pks@delegate.dk>
2023-10-17 11:54:26 +0200

    fix: fixed naming violation

--- src/WorkflowEngine.Core/ActionExecutor.cs
+++ src/WorkflowEngine.Core/ActionExecutor.cs
@@ -1,4 +1,4 @@
-﻿using ExpressionEngine;
+using ExpressionEngine;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
@@ -18,32 +18,32 @@ namespace WorkflowEngine.Core
     }
     public class ActionExecutor : IActionExecutor
     {
-        private readonly IOutputsRepository outputsRepository;
-        private readonly IServiceProvider serviceProvider;
-        private readonly ILogger logger;
-        private readonly IScopeContext scopeContext;
-        private readonly IExpressionEngine expressionEngine;
+        private readonly IOutputsRepository _outputsRepository;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ILogger _logger;
+        private readonly IScopeContext _scopeContext;
+        private readonly IExpressionEngine _expressionEngine;
         private Dictionary<string, IActionImplementationMetadata> _implementations;
 
         public ActionExecutor(
-            IEnumerable<IActionImplementationMetadata> implementations, 
+            IEnumerable<IActionImplementationMetadata> implementations,
             IOutputsRepository outputsRepository,
             IServiceProvider serviceProvider,
             ILogger<ActionExecutor> logger,
             IScopeContext scopeContext,
             IExpressionEngine expressionEngine)
         {
-           
-            if(implementations.GroupBy(k=>k.Type).Any(c=>c.Count() > 1))
+
+            if (implementations.GroupBy(k => k.Type).Any(c => c.Count() > 1))
             {
-                throw new ArgumentException("Double registration of " + String.Join(",", implementations.GroupBy(k => k.Type).Where(c => c.Count() > 1).Select(c=>c.Key)));
+                throw new ArgumentException("Double registration of " + String.Join(",", implementations.GroupBy(k => k.Type).Where(c => c.Count() > 1).Select(c => c.Key)));
             }
             _implementations = implementations?.ToDictionary(k => k.Type) ?? throw new ArgumentNullException(nameof(implementations));
-            this.outputsRepository=outputsRepository??throw new ArgumentNullException(nameof(outputsRepository));
-            this.serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
-            this.logger=logger??throw new ArgumentNullException(nameof(logger));
-            this.scopeContext=scopeContext;
-            this.expressionEngine=expressionEngine??throw new ArgumentNullException(nameof(expressionEngine));
+            _outputsRepository = outputsRepository ?? throw new ArgumentNullException(nameof(outputsRepository));
+            _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _scopeContext = scopeContext;
+            _expressionEngine = expressionEngine ?? throw new ArgumentNullException(nameof(expressionEngine));
         }
         public async ValueTask<IActionResult> ExecuteAsync(IRunContext context, IWorkflow workflow, IAction action)
         {
@@ -52,13 +52,13 @@ namespace WorkflowEngine.Core
 
                 if (action.ScopeMoveNext)
                 {
-                    await outputsRepository.EndScope(context, workflow, action);
+                    await _outputsRepository.EndScope(context, workflow, action);
                 }
 
                 var actionMetadata = workflow.Manifest.Actions.FindAction(action.Key);
-                scopeContext.Scope=action.Key;
-                action.Inputs = await expressionEngine.ResolveInputs(actionMetadata,logger);
-               
+                _scopeContext.Scope = action.Key;
+                action.Inputs = await _expressionEngine.ResolveInputs(actionMetadata, _logger);
+
                 {
                     //if (workflow.Manifest.Actions.FindParentAction(action.Key) is ForLoopActionMetadata parent)
                     //{
@@ -71,37 +71,39 @@ namespace WorkflowEngine.Core
                     //}
                     //else
                     {
-                        await outputsRepository.AddInput(context, workflow, action);
+                        await _outputsRepository.AddInput(context, workflow, action);
                     }
                 }
-                 
-                var actionImplementation = serviceProvider.GetRequiredService(_implementations[actionMetadata.Type].Implementation) as IActionImplementation;
-                 
+
+                var actionImplementation = _serviceProvider.GetRequiredService(_implementations[actionMetadata.Type].Implementation) as IActionImplementation;
 
 
 
-                var result = new ActionResult { 
-                    Key = action.Key, 
-                    Status = "Succeded", 
-                    Result = await actionImplementation.ExecuteAsync(context,workflow, action) 
+
+                var result = new ActionResult
+                {
+                    Key = action.Key,
+                    Status = "Succeded",
+                    Result = await actionImplementation.ExecuteAsync(context, workflow, action)
                 };
 
-                
-                    
-                        await outputsRepository.AddAsync(context, workflow, action, result);
-                 
+
+
+                await _outputsRepository.AddAsync(context, workflow, action, result);
+
 
                 return result;
-           
-            
-            }catch(Exception ex)
+
+
+            }
+            catch (Exception ex)
             {
-                var result= new ActionResult { Key = action.Key, Status = "Failed", FailedReason=ex.ToString(), ReThrow = (ex is  InvalidOperationException) };
+                var result = new ActionResult { Key = action.Key, Status = "Failed", FailedReason = ex.ToString(), ReThrow = (ex is InvalidOperationException) };
                 try
                 {
-                    await outputsRepository.AddAsync(context, workflow, action, result);
+                    await _outputsRepository.AddAsync(context, workflow, action, result);
                 }
-                catch (Exception )
+                catch (Exception exx)
                 {
 
                 }
@@ -109,7 +111,7 @@ namespace WorkflowEngine.Core
             }
         }
     }
-    
+
 
 
 }
