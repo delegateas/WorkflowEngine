73d2afd15f71f5a98a627542e7e3852c696fbbe1
Poul Kjeldager Sørensen <pks@delegate.dk>
2023-11-27 14:29:28 +0100

    fix: exposed HangfireWorkflowManifestJobFilter to DI

--- src/WorkflowEngine.Hangfire/DependencyInjectionExtensions.cs
+++ src/WorkflowEngine.Hangfire/DependencyInjectionExtensions.cs
@@ -32,6 +32,7 @@ namespace Microsoft.Extensions.DependencyInjection
 
             services.AddTransient<IWorkflowAccessor, DefaultWorkflowAccessor>();
             services.AddTransient<IHangfireActionExecutorResultInspector, DefaultHangfireActionExecutorResultInspector>();
+            services.AddTransient<HangfireWorkflowManifestJobFilter>();
 
             return services;
         }
